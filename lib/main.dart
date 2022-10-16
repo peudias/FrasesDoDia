db193546be12f6a22fa35f0448630b89ab1237a3
p8 <phpdias@outlook.com>
2022-10-15 22:43:14 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Frases do dia'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _numeroAleatorio = 0;
+  List _frases = [
+    'O Brasil será hexa em 2022.',
+    'Naruto é melhor que Dragon Ball.',
+    'Minecraft é um jogo atemporal.',
+    'Um salve para os fifeiros. Viva a web pelada ;9',
+    'Michelle eu te amo!',
+    'Quem é esse tal de P que sempre dá assistência pro Hulk?',
+    '87 é do Sport',
+    'Wright estava certo.',
+    'Maíra não é uma boa sugarmommy',
+    'F*deu, ano que vem tem Física 3'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+      _numeroAleatorio = new Random().nextInt(9);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pressione o botão para gerar uma frase',
+            ),
+            Text(
+              _frases[_numeroAleatorio],
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
